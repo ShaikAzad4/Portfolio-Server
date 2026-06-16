9860c5d341000a94e32258adec7db6d6f970da5c
ShaikAzad4 <shaikazad3131@gmail.com>
2026-06-16 12:23:03 +0530

    Use Resend for emails

--- portfolioapp/views.py
+++ portfolioapp/views.py
@@ -1,37 +1,37 @@
-from django.views.decorators.csrf import csrf_exempt
-from django.utils.decorators import method_decorator
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from django.core.mail import send_mail
 
+from rest_framework.response import Response
+from rest_framework.views import APIView
 import os
 import resend
-
-from rest_framework.views import APIView
-from rest_framework.response import Response
 
 resend.api_key = os.environ.get("RESEND_API_KEY")
 
 class Send_mail_api(APIView):
 
     def post(self, request):
+        try:
+            name = request.data.get("name")
+            client_mail = request.data.get("mail")
+            description = request.data.get("desc")
 
-        name = request.data.get("name")
-        client_mail = request.data.get("mail")
-        description = request.data.get("desc")
+            result = resend.Emails.send({
+                "from": "onboarding@resend.dev",
+                "to": ["shaikazad3131@gmail.com"],
+                "subject": f"Portfolio Contact From {name}",
+                "html": f"""
+                <h3>Portfolio Contact</h3>
+                <p>Name: {name}</p>
+                <p>Email: {client_mail}</p>
+                <p>Message: {description}</p>
+                """
+            })
 
-        resend.Emails.send({
-            "from": "onboarding@resend.dev",
-            "to": ["shaikazad3131@gmail.com"],
-            "subject": f"Portfolio Contact From {name}",
-            "html": f"""
-            <h3>Portfolio Contact</h3>
-            <p><b>Name:</b> {name}</p>
-            <p><b>Email:</b> {client_mail}</p>
-            <p><b>Message:</b> {description}</p>
-            """
-        })
+            return Response({
+                "message": "success",
+                "result": str(result)
+            })
 
-        return Response({
-            "message": "Mail sent successfully"
-        })
+        except Exception as e:
+            return Response({
+                "error": str(e)
+            }, status=500)
